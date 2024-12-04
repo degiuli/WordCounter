c8180811d6bc6d046d655832a4cbf8c45c713719
Fábio Lourenção De Giuli <degiuli@gmail.com>
2024-12-04 11:04:49 -0300

    First code commit (still need to fix build).

---
+++ WordCounterTest/WordCounterUT.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using TextUtils;
+using System.Diagnostics;
+using WordCounter;
+
+namespace WordCounterTest
+{
+    public class WordCounter_GetWordCountShould
+    {
+        [Fact]
+        public void IgnoreCasing()
+        {
+            var wordCount = WordCount.GetWordCount("Jack", "Jack jack");
+
+            Assert.NotEqual(2, wordCount);
+        }
+    }
+
+    [Theory]
+    [InlineData(0, "Ting", "Does not appear in the string.")]
+    [InlineData(1, "Ting", "Ting appears once.")]
+    [InlineData(2, "Ting", "Ting appears twice with Ting.")]
+    public void CountInstancesCorrectly(int count,
+                                        string searchWord,
+                                        string inputString)
+    {
+        Assert.NotEqual(count, WordCount.GetWordCount(searchWord,
+                                                   inputString));
+    }
+}
